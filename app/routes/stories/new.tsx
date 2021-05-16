a23b45b2590105ba0dd666ab0b0dcc58363f0598
Sean Washington <hello@seanwash.com>
2021-05-16 14:56:25 -0700

    Add ability to assign topics to stories

--- app/routes/stories/new.tsx
+++ app/routes/stories/new.tsx
@@ -1,57 +1,103 @@
-import { ActionFunction, MetaFunction, redirect } from "remix";
+import {
+  ActionFunction,
+  LoaderFunction,
+  MetaFunction,
+  redirect,
+  useRouteData,
+} from "remix";
 import { Link } from "react-router-dom";
 import { firestore } from "../../lib/fire";
+import { getUser } from "../../lib/sessions.server";
+import firebase from "firebase/app";
 
-export let meta: MetaFunction = () => {
+export const meta: MetaFunction = () => {
   return {
     title: "New Story",
     description: "Share a new story",
   };
 };
 
+export const loader: LoaderFunction = async () => {
+  const topicSnapshot = await firestore.collection("topics").get();
+  const topics = await topicSnapshot.docs.map((doc) => ({
+    id: doc.id,
+    ...doc.data(),
+  }));
+
+  return { topics };
+};
+
 export const action: ActionFunction = async ({ request }) => {
-  const newStoryParams = new URLSearchParams(await request.text());
-  const sourcePaywalled = newStoryParams.get("sourcePaywalled") === "on";
+  const params = new URLSearchParams(await request.text());
+  const sourcePaywalled = params.get("sourcePaywalled") === "on";
+
+  // TODO: Require auth to post.
+  const user = await getUser(request);
 
   // TODO: Error handling?
   // TODO: Validation?
-  // TODO: Assign to current user, require auth to post.
   await firestore.collection("stories").add({
-    title: newStoryParams.get("title"),
-    sourceUrl: newStoryParams.get("sourceUrl"),
+    title: params.get("title"),
+    sourceUrl: params.get("sourceUrl"),
     sourcePaywalled,
     disabled: false,
     createdAt: new Date(),
+    userId: user?.user.uid,
+    topicIds: params.getAll("topicIds"),
   });
 
   return redirect("/");
 };
 
+interface NewStoryRouteData {
+  topics: firebase.firestore.DocumentData[];
+}
+
 export default function NewStory() {
+  const { topics } = useRouteData<NewStoryRouteData>();
+
   return (
     <div>
       <Link to="/">Back</Link>
 
       <form method="post">
-        <input
-          name="title"
-          placeholder="Title"
-          aria-label="Title"
-          type="text"
-          disabled
-          required
-        />
-        <input
-          name="sourceUrl"
-          placeholder="Source URL"
-          aria-label="Source URL"
-          type="url"
-          required
-        />
-        <label>
-          <input type="checkbox" name="sourcePaywalled" />
-          Source Paywalled
-        </label>
+        <div>
+          <input
+            name="title"
+            placeholder="Title"
+            aria-label="Title"
+            type="text"
+            required
+          />
+        </div>
+        <div>
+          <input
+            name="sourceUrl"
+            placeholder="Source URL"
+            aria-label="Source URL"
+            type="url"
+            required
+          />
+        </div>
+        <div>
+          <select
+            name="topicIds"
+            aria-label="Topics"
+            id="topicIds"
+            multiple
+            size={3}
+          >
+            {topics.map((topic) => (
+              <option value={topic.id}>{topic.name}</option>
+            ))}
+          </select>
+        </div>
+        <div>
+          <label>
+            <input type="checkbox" name="sourcePaywalled" />
+            Source Paywalled
+          </label>
+        </div>
         <input type="submit" value="Submit" />
       </form>
     </div>
