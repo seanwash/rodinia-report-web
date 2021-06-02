71c6b7ff17ca2b374e3c76ff02e773e6ad08abab
Sean Washington <hello@seanwash.com>
2021-06-01 20:27:36 -0700

    Add viewport meta tag

--- app/root.tsx
+++ app/root.tsx
@@ -26,6 +26,7 @@ function Document({ children }: { children: React.ReactNode }) {
     <html lang="en">
       <head>
         <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" type="image/x-icon" />
         <Meta />
         <Links />
