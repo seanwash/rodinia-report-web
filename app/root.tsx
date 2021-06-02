710260fc2a4c3e9a29efd39ad2e1bfc899c6bb89
Sean Washington <hello@seanwash.com>
2021-06-01 20:45:23 -0700

    More mobile tweaks, update tailwind, fix order bug with tailwind utilities

--- app/root.tsx
+++ app/root.tsx
@@ -51,24 +51,6 @@ function Document({ children }: { children: React.ReactNode }) {
           </div>
         </div>
 
-        <div className="container mx-auto sm:flex items-center justify-between p-4">
-          <div className="flex-1 min-w-0">
-            <h2 className="text-2xl leading-7 text-el-paso sm:text-3xl sm:leading-9 max-w-4xl">
-              The <span className="font-bold">Rodinia Report</span> is a public
-              curation of environmentally focused articles that helps
-              individuals easily stay up to date on the most recent and most
-              inspiring undertakings around the world.
-            </h2>
-          </div>
-          <div className="flex mt-4 sm:mt-0 sm:ml-8">
-            <span className="shadow-sm rounded-sm">
-              <Link className="twc-button" to="/stories/new">
-                Submit Story
-              </Link>
-            </span>
-          </div>
-        </div>
-
         <main className="container mx-auto p-4">{children}</main>
 
         <script
